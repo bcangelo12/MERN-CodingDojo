0f95ec36266dde1011f7a3b0322cb87381b72efb
Brendan Angelo <brendanangelo12@gmail.com>
2022-04-18 17:04:02 -0700

    initial implementation of react bootstrap

--- Week2/practice/hook_form/src/App.js
+++ Week2/practice/hook_form/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Form from "./components/Form";
+import UserForm from "./components/Form";
 
 function App() {
     return (
         <div className="App">
-            <Form />
+            <UserForm />
         </div>
     );
 }
--- Week2/practice/hook_form/src/components/Form.js
+++ Week2/practice/hook_form/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 const UserForm = (props) => {
     const [firstName, setFirstName] = useState("");
@@ -26,48 +27,65 @@ const UserForm = (props) => {
 
     return (
         <div>
-            <form onSubmit={createUser}>
-                <div>
-                    <label>First Name: </label>
-                    <input
+            <Form onSubmit={createUser}>
+                <Form.Group className="mb-3" controlId="firstName">
+                    <Form.Label>First Name: </Form.Label>
+                    <Form.Control
                         type="text"
+                        placeholder="First Name"
+                        name="firstName"
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                     />
-                </div>
-                <div>
-                    <label>Last Name: </label>
-                    <input
+                </Form.Group>
+                {/* <input
+                        type="text"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    /> */}
+                <Form.Group className="mb-3" controlId="lastName">
+                    <Form.Label>Last Name: </Form.Label>
+                    <Form.Control
                         type="text"
                         value={lastName}
                         onChange={(e) => setLastName(e.target.value)}
+                        name="lastName"
+                        placeholder="Last Name"
                     />
-                </div>
-                <div>
-                    <label>Email: </label>
-                    <input
+                </Form.Group>
+                {/* <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                /> */}
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control
                         type="text"
+                        placeholder="Email"
+                        name="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
-                </div>
-                <div>
-                    <label>Password: </label>
-                    <input
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control
                         type="password"
+                        name="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                </div>
-                <div>
-                    <label>Confirm Password: </label>
-                    <input
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Confirm Password: </Form.Label>
+                    <Form.Control
                         type="password"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     />
-                </div>
-            </form>
+                </Form.Group>
+            </Form>
             <div>
                 <p>Your Form Data:</p>
                 <p>
