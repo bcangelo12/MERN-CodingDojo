f2c88f9b971caea24515b23c601222f568bad753
Brendan Angelo <brendanangelo12@gmail.com>
2022-05-11 17:02:23 -0700

    finalized product manager pt 3

--- Week6/Product_Manager/client/src/components/Detail.js
+++ Week6/Product_Manager/client/src/components/Detail.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Detail = (props) => {
     const [product, setProduct] = useState({});
     const { id } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
@@ -19,12 +22,31 @@ const Detail = (props) => {
             });
     }, [id]);
 
+    const deleteHandler = () => {
+        axios
+            .delete(`http://localhost:8000/api/products/${id}`)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(
+                    "Something went wrong deleting selected product",
+                    err
+                );
+            });
+    };
+
     return (
         <div>
             <h2>Title: {product.title}</h2>
             <p>Price: {product.price}</p>
             <p>Description: {product.description}</p>
             <Link to="/">Back to Home</Link>
+            <Button onClick={deleteHandler} className="mx-2" variant="danger">
+                Delete
+            </Button>
         </div>
     );
 };
